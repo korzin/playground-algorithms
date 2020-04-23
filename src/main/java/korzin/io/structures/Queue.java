e14e4b3191b1b07e2b255491661b7e74e13d28f2
korzin <Samkorzin@gmail.com>
2020-04-23 10:45:07 +0300

    Structures - queue - upd

--- src/main/java/korzin/io/structures/Queue.java
+++ src/main/java/korzin/io/structures/Queue.java
@@ -4,18 +4,22 @@ public class Queue<T> {
 
     private TwoDirLList<T> linkedList = new TwoDirLList<>();
 
-    public void push(T value) {
+    public void enqueue(T value) {
         linkedList.pushFront(value);
     }
 
-    public T top() {
-        TwoDirLList.Node<T> tail = linkedList.topBack();
-        return tail == null ? null : tail.getValue();
+    public boolean empty() {
+        return top() == null;
     }
 
-    public T pop() {
+    public T dequeue() {
         T tail = top();
         linkedList.popBack();
         return tail;
+    }
+
+    private T top() {
+        TwoDirLList.Node<T> tail = linkedList.topBack();
+        return tail == null ? null : tail.getValue();
     }
 }
--- src/test/java/korzin/io/structures/QueueTest.java
+++ src/test/java/korzin/io/structures/QueueTest.java
@@ -2,32 +2,29 @@ package korzin.io.structures;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class QueueTest {
 
     @Test
-    public void testQueue(){
+    public void testQueue() {
 
         Queue<String> queue = new Queue<>();
 
-        queue.push("111");
-        queue.pop();
-        queue.push("111");
-        queue.push("222");
-        queue.push("333");
-        queue.push("444");
-        queue.top();
-        queue.top();
+        queue.enqueue("111");
+        assertFalse(queue.empty());
+        queue.dequeue();
+        assertTrue(queue.empty());
+        queue.enqueue("111");
+        queue.enqueue("222");
+        queue.enqueue("333");
+        queue.enqueue("444");
 
-        queue.pop();
+        queue.dequeue();
 
-        assertEquals("222", queue.top());
-        assertEquals("222", queue.pop());
-        assertEquals("333", queue.pop());
-        assertEquals("444", queue.pop());
-
+        assertEquals("222", queue.dequeue());
+        assertEquals("333", queue.dequeue());
+        assertEquals("444", queue.dequeue());
 
     }
-
 }
