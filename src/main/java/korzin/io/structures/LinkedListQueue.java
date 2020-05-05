7606f453942b6db7eba143ed126f1d159529557e
korzin <Samkorzin@gmail.com>
2020-05-05 20:35:02 +0300

    Structures - LinkedListQueue.fromList

--- src/main/java/korzin/io/structures/LinkedListQueue.java
+++ src/main/java/korzin/io/structures/LinkedListQueue.java
@@ -1,28 +1,38 @@
 package korzin.io.structures;
 
+import java.util.List;
+
 public class LinkedListQueue<T> implements Queue<T> {
 
-    private TwoDirLList<T> linkedList = new TwoDirLList<>();
+  private TwoDirLList<T> linkedList = new TwoDirLList<>();
 
-    @Override
-    public void enqueue(T value) {
-        linkedList.pushFront(value);
+  public static <TT> LinkedListQueue<TT> fromList(List<TT> list) {
+    LinkedListQueue<TT> queue = new LinkedListQueue<>();
+    for (TT el : list) {
+      queue.enqueue(el);
     }
+    return queue;
+  }
 
-    @Override
-    public boolean empty() {
-        return top() == null;
-    }
+  @Override
+  public void enqueue(T value) {
+    linkedList.pushFront(value);
+  }
 
-    @Override
-    public T dequeue() {
-        T tail = top();
-        linkedList.popBack();
-        return tail;
-    }
+  @Override
+  public boolean empty() {
+    return top() == null;
+  }
 
-    private T top() {
-        TwoDirLList.Node<T> tail = linkedList.topBack();
-        return tail == null ? null : tail.getValue();
-    }
+  @Override
+  public T dequeue() {
+    T tail = top();
+    linkedList.popBack();
+    return tail;
+  }
+
+  private T top() {
+    TwoDirLList.Node<T> tail = linkedList.topBack();
+    return tail == null ? null : tail.getValue();
+  }
 }
