4e5dbfc6cdc07ba2191e665f2d39871b60669c97
korzin <Samkorzin@gmail.com>
2020-05-05 19:33:38 +0300

    Structures - Stack.fromList

--- src/main/java/korzin/io/structures/Stack.java
+++ src/main/java/korzin/io/structures/Stack.java
@@ -1,5 +1,7 @@
 package korzin.io.structures;
 
+import java.util.List;
+
 public class Stack<T> {
 
     private TwoDirLList<T> linkedList = new TwoDirLList<>();
@@ -17,6 +19,14 @@ public class Stack<T> {
         T head = top();
         linkedList.popFront();
         return head;
+    }
+
+    public static <ST> Stack<ST> fromList(List<ST> list){
+        Stack<ST> stack = new Stack<>();
+        for(ST el : list){
+            stack.push(el);
+        }
+        return stack;
     }
 
 }
